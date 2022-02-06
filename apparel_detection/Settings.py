6f58cf1a32217c3b9e0b2d9003f0d9253ebe2f4c
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2022-02-06 12:22:03 +0530

    added seetings file for model configuration

---
+++ apparel_detection/Settings.py
@@ -0,0 +1,4 @@
+class Settings:
+    input_image_path = "./apparel_detection/yolo_detector/inference/images/"
+    weights_path = "./apparel_detection/yolo_detector/weights/best.pt"
+    output_path = "./apparel_detection/yolo_detector/inference/output/"
